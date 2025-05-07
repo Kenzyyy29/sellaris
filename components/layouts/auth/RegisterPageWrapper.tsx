10dacc4088fcb6de44d0df8e2dd7657c58a593a6
Kenzyyy29 <kenzyworld29@gmail.com>
2025-05-07 21:25:49 +0700

    fix:error-deployment 8

--- components/layouts/auth/RegisterPageWrapper.tsx
+++ components/layouts/auth/RegisterPageWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 import RegisterForm from "@/components/core/modal/RegisterForm";
-import {useRouter} from "next/router";
+import {useRouter} from "next/navigation";
 
 export default function RegisterPageWrapper() {
  const {push} = useRouter();
